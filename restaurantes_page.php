b146588da4dac74f296235934c54d2b53a42b96e
Richard Miranda <116800106+richardmiranda110@users.noreply.github.com>
2024-05-03 12:04:25 +0100

    Update restaurantes_page.php

--- restaurantes_page.php
+++ restaurantes_page.php
@@ -1,3 +1,91 @@
+<?php
+//PESQUISAR RESTAURANTE
+/*
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $restaurante = $_POST["input_pesquisar_restaurante"];
+
+  try {
+    if (!empty($restaurante)) {
+      $q = "SELECT nome, imagem, avaliacao, taxa_entrega, tempo_medio_entrega FROM restaurantes WHERE nome LIKE ?";
+      $statement = $pdo->prepare($q);
+      $statement->execute(array("%$restaurante%"));
+
+      if ($statement) {
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($result) {
+          foreach ($result as $row) {
+            echo '<div class="col">
+                  <div class="card shadow-sm">
+                    <img src="' . $row["imagem"] . '" class="card-img-top" alt="Imagem do restaurante" style="border-radius: 5.5px;">
+                    <div class="card-body">
+                      <div class="d-flex justify-content-between align-items-center">
+                        <h5 class="mb-0">' . $row["nome"] . '</h5>
+                        <p class="mb-0">' . $row["avaliacao"] . '★</p>
+                      </div>
+
+                      <div class="d-flex justify-content-between align-items-center">
+                        <p class="card-text mb-0" style="font-size: 12px;">Taxa de Entrega: ' . $row["taxa_entrega"] . '€</p>
+                        <small class="text-body-secondary mb-0" style="font-size: 12px;">' . $row["tempo_medio_entrega"] . ' mins</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>';
+          }
+        } else {
+          echo "Restaurante não encontrado :(";
+        }
+      } else {
+        echo "Erro ao executar a consulta.";
+      }
+    }
+  } catch (Exception $e) {
+    echo "Erro na conexão à BD: " . $e->getMessage();
+  }
+}*/
+?>
+
+<?php
+//POPULAR PÁGINA COM OS RESTAURANTES DA BASE DE DADOS
+/*
+try {
+
+  $q = "SELECT nome, imagem, avaliacao, taxa_entrega, tempo_medio_entrega FROM restaurantes;";
+  $statement = $pdo->prepare($q);
+  $statement->execute();
+
+  if ($statement) {
+    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    if ($result) {
+      foreach ($result as $row) {
+        echo '<div class="col">
+                <div class="card shadow-sm">
+                  <img src="' . $row["imagem"] . '" class="card-img-top" alt="Imagem do restaurante" style="border-radius: 5.5px;">
+                  <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center">
+                      <h5 class="mb-0">' . $row["nome"] . '</h5>
+                      <p class="mb-0">' . $row["avaliacao"] . '★</p>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <p class="card-text mb-0" style="font-size: 12px;">Taxa de Entrega: ' . $row["taxa_entrega"] . '€</p>
+                      <small class="text-body-secondary mb-0" style="font-size: 12px;">' . $row["tempo_medio_entrega"] . ' mins</small>
+                    </div>
+                  </div>
+                </div>
+              </div>';
+      }
+    }
+  } else {
+    echo "Nenhum restaurante encontrado :(";
+  }
+} catch (Exception $e) {
+  echo "Erro na conexão à BD: " . $e->getMessage();
+}*/
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="pt">
 
@@ -25,10 +113,10 @@
   <br><br>
   <!-- TÍTULO PÁGINA E PROCURAR -->
   <h1 style="text-align: center;">Restaurantes</h1><br>
-  <div class="input-group container text-center mb-5" style="max-width: 40%;">
-    <input type="text" class="form-control" placeholder="Procurar restaurante" id="inputPesquisarRestaurante">
+  <form class="input-group container text-center mb-5" style="max-width: 40%;" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <input type="text" class="form-control" placeholder="Procurar restaurante" name="input_pesquisar_restaurante" id="inputPesquisarRestaurante">
     <button class="btn btn-outline-primary" type="button" id="buttonPesquisarRestaurante">Procurar</button>
-  </div>
+  </form>
 
   <br>
 
@@ -557,13 +645,11 @@
       window.location.href = 'menu_restaurante_page.php';
     });
 
-    document.querySelector("button#buttonPesquisarRestaurante").addEventListener("click", procurarRestaurante);
-
-    function procurarRestaurante() {
+    //PESQUISAR RESTAURANTE (acho que este código não vai servir pra nada)
+    document.querySelector("button#buttonPesquisarRestaurante").addEventListener("click", function() {
       let nomeRestaurante = document.querySelector("input#inputPesquisarRestaurante").value;
-      console.log(nomeRestaurante)
       document.querySelector("input#inputPesquisarRestaurante").value = "";
-    }
+    });
   </script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
