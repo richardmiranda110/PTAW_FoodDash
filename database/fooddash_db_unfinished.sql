68047ed3cb65ed67087328e9ab3ebdd1d6f07644
Richard Miranda <116800106+richardmiranda110@users.noreply.github.com>
2024-05-05 22:40:41 +0100

    melhoramento populate

--- database/fooddash_db_unfinished.sql
+++ database/fooddash_db_unfinished.sql
@@ -1,6 +1,6 @@
 -- Tabela Cliente
 CREATE TABLE IF NOT EXISTS Clientes (
-    id BIGSERIAL PRIMARY KEY,
+    id_cliente BIGSERIAL PRIMARY KEY,
     nome VARCHAR(100),
     apelido VARCHAR(100),
     email VARCHAR(100) UNIQUE NOT NULL,
@@ -14,20 +14,20 @@ CREATE TABLE IF NOT EXISTS Clientes (
 
 -- Tabela Pedido
 CREATE TABLE IF NOT EXISTS Pedidos (
-    id BIGSERIAL PRIMARY KEY,
+    id_pedido BIGSERIAL PRIMARY KEY,
     data DATE DEFAULT NOW(),
     estado VARCHAR(50) DEFAULT 'EFETUADO'
     CHECK(estado IN ('EFETUADO','EM PREPARACAO','A CAMINHO','FINALIZADO')),
     cancelado BOOLEAN DEFAULT FALSE,
     precoTotal DECIMAL(10,2) NOT NULL,
-    id_cliente INTEGER REFERENCES Clientes(id) ON DELETE CASCADE NOT NULL,
+    id_cliente INTEGER REFERENCES Clientes(id_cliente) ON DELETE CASCADE NOT NULL,
     id_entregador INTEGER NOT NULL,
     id_estabelecimento INTEGER NOT NULL
 );
 
 -- Tabela Entregador
 CREATE TABLE IF NOT EXISTS Entregadores (
-    id BIGSERIAL PRIMARY KEY,
+    id_entregador BIGSERIAL PRIMARY KEY,
     nome VARCHAR(100),
     veiculo VARCHAR(100)
 );
@@ -37,25 +37,24 @@ CREATE TABLE IF NOT EXISTS ACTION_LOGGER
     id BIGSERIAL PRIMARY KEY,
     AFFECTED_ID INT NOT NULL,
     AFFECTED_NOTE INT,
-    ACTION_TYPE VARCHAR(30) NOT NULL
-     CHECK(ACTION_TYPE IN ('UPDATE NOTE','DELETE NOTE','DELETE USER')),
+    ACTION_TYPE VARCHAR(30) NOT NULL CHECK(ACTION_TYPE IN ('UPDATE NOTE','DELETE NOTE','DELETE USER')),
     ACTION_DATE TIMESTAMPTZ DEFAULT NOW()
 );
 
 -- Tabela AvaliacaoItem
 CREATE TABLE IF NOT EXISTS AvaliacoesItens (
-    id BIGSERIAL PRIMARY KEY,
+    id_avaliacaoItem BIGSERIAL PRIMARY KEY,
     classificacao INTEGER,
     autor VARCHAR(100),
     data DATE,
     descricao TEXT,
-    id_cliente INTEGER REFERENCES Clientes(id) ON DELETE CASCADE NOT NULL,
+    id_cliente INTEGER REFERENCES Clientes(id_cliente) ON DELETE CASCADE NOT NULL,
     id_item INTEGER NOT NULL
 );
 
 -- Tabela Empresa
 CREATE TABLE IF NOT EXISTS Empresas (
-    id BIGSERIAL PRIMARY KEY,
+    id_empresa BIGSERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     morada TEXT NOT NULL,
     telemovel VARCHAR(20) NOT NULL,
@@ -66,26 +65,25 @@ CREATE TABLE IF NOT EXISTS Empresas (
 
 -- Tabela Estabelecimento
 CREATE TABLE IF NOT EXISTS Estabelecimentos (
-    id BIGSERIAL PRIMARY KEY,
+    id_estabelecimento BIGSERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     localizacao TEXT NOT NULL,
     telemovel VARCHAR(20) NOT NULL,
     taxa_entrega DECIMAL NOT NULL,
     tempo_medio_entrega TIME NOT NULL,
-    id_empresa INTEGER
-    REFERENCES Empresas(id)
-    ON DELETE CASCADE NOT NULL
+    id_empresa INTEGER REFERENCES Empresas(id_empresa) ON DELETE CASCADE NOT NULL
 );
 
 -- Tabela Categoria
 CREATE TABLE IF NOT EXISTS Categorias (
-    id BIGSERIAL PRIMARY KEY,
-    nome VARCHAR(100)
+    id_categoria BIGSERIAL PRIMARY KEY,
+    nome VARCHAR(100),
+    id_empresa INTEGER REFERENCES Empresas(id_empresa) ON DELETE CASCADE NOT NULL
 );
 
 -- Tabela Item
 CREATE TABLE IF NOT EXISTS Itens (
-    id BIGSERIAL PRIMARY KEY,
+    id_item BIGSERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     preco DECIMAL(10,2) NOT NULL,
     descricao TEXT,
@@ -93,30 +91,28 @@ CREATE TABLE IF NOT EXISTS Itens (
     foto VARCHAR(255),
     itemSozinho BOOLEAN DEFAULT FALSE,
     personalizacoesAtivas BOOLEAN DEFAULT FALSE,
-    id_categoria INTEGER NOT NULL REFERENCES Categorias(id) ON DELETE CASCADE NOT NULL,
-    id_estabelecimento INTEGER 
-    REFERENCES Estabelecimentos(id)
-    ON DELETE CASCADE NOT NULL
-);
-
--- Tabela Opcao
-CREATE TABLE IF NOT EXISTS Opcoes (
-    id BIGSERIAL PRIMARY KEY,
-    nome VARCHAR(100),
-    quantidade INTEGER
+    id_categoria INTEGER NOT NULL REFERENCES Categorias(id_categoria) ON DELETE CASCADE NOT NULL,
+    id_estabelecimento INTEGER REFERENCES Estabelecimentos(id_estabelecimento) ON DELETE CASCADE NOT NULL
 );
 
 -- Tabela Personalizacao
 CREATE TABLE IF NOT EXISTS Personalizacoes (
-    id BIGSERIAL PRIMARY KEY,
-    nome VARCHAR(100),
-    id_opcao INTEGER REFERENCES Opcoes(id) 
-    ON DELETE CASCADE,
-    id_item INTEGER REFERENCES Itens(id)
-    ON DELETE CASCADE NOT NULL
+    id_personalizacao BIGSERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    id_item INTEGER REFERENCES Itens(id_item) ON DELETE CASCADE NOT NULL
 );
 
--- Tabela Menu
+-- Tabela Opcao
+CREATE TABLE IF NOT EXISTS Opcoes (
+    id_opcao BIGSERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    quantidade INTEGER,
+    id_personalizacao INTEGER REFERENCES Personalizacoes(id_personalizacao) ON DELETE CASCADE NOT NULL
+);
+
+
+
+/* -- Tabela Menu
 CREATE TABLE IF NOT EXISTS Menus (
     id BIGSERIAL PRIMARY KEY,
     nome VARCHAR(100),
@@ -125,53 +121,40 @@ CREATE TABLE IF NOT EXISTS Menus (
     id_estabelecimento INTEGER
     REFERENCES Estabelecimentos(id)
     ON DELETE CASCADE NOT NULL
-);
+); */
 
 -- Tabela Avaliacao
 CREATE TABLE IF NOT EXISTS Avaliacoes (
-    id BIGSERIAL PRIMARY KEY,
+    id_avaliacao BIGSERIAL PRIMARY KEY,
     classificacao INTEGER,
     autor VARCHAR(100),
     data DATE,
     descricao TEXT,
-    id_cliente INTEGER
-    REFERENCES Clientes(id)
-    ON DELETE CASCADE NOT NULL,
-    id_estabelecimento INTEGER
-    REFERENCES Estabelecimentos(id)
-    ON DELETE CASCADE NOT NULL
+    id_cliente INTEGER REFERENCES Clientes(id_cliente) ON DELETE CASCADE NOT NULL,
+    id_estabelecimento INTEGER REFERENCES Estabelecimentos(id_estabelecimento) ON DELETE CASCADE NOT NULL
 );
 
 -- Tabelas de Associação
 
 -- Tabela Pedido_Item
 CREATE TABLE IF NOT EXISTS Pedido_Itens (
-    id_pedido INTEGER 
-    REFERENCES Pedidos(id) ON DELETE
-    CASCADE NOT NULL,
-    id_item INTEGER REFERENCES Itens(id) 
-    ON DELETE CASCADE NOT NULL,
+    id_pedido INTEGER REFERENCES Pedidos(id) ON DELETE CASCADE NOT NULL,
+    id_item INTEGER REFERENCES Itens(id) ON DELETE CASCADE NOT NULL,
     PRIMARY KEY (id_pedido, id_item)
 );
 
 -- Tabela Item_Categoria
 CREATE TABLE IF NOT EXISTS Item_Categorias (
-    id_item INTEGER REFERENCES Itens(id) 
-    ON DELETE CASCADE NOT NULL,
-    id_categoria INTEGER REFERENCES Categorias(id) 
-    ON DELETE CASCADE NOT NULL,
+    id_item INTEGER REFERENCES Itens(id) ON DELETE CASCADE NOT NULL,
+    id_categoria INTEGER REFERENCES Categorias(id) ON DELETE CASCADE NOT NULL,
     PRIMARY KEY (id_item, id_categoria)
 );
 
--- Tabela Item_Menu
+/* -- Tabela Item_Menu
 CREATE TABLE IF NOT EXISTS Item_Menus (
     id_item INTEGER REFERENCES Itens(id)
     ON DELETE CASCADE NOT NULL,
     id_menu INTEGER REFERENCES Menus(id) 
     ON DELETE CASCADE NOT NULL,
     PRIMARY KEY (id_item, id_menu)
-);
-
--- insersão de dados de teste, para testar código
-INSERT INTO Clientes (id, nome, apelido, email, telemovel, morada, cidade, pais, CodPostal, password) 
-VALUES (1, "nome", "apelido", "email", 123456789, "morada", "cidade", "pais", "codigo-postal", "password");
+); */
