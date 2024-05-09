aca27a76d80989f9715f54f0740086fe58e80e7e
Ana Beatriz Vicente <vicente.anab@ua.pt>
2024-05-09 20:37:14 +0100

    tentando corrigir o estilo do perfil.php

--- perfil.php
+++ perfil.php
@@ -41,13 +41,43 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Editar o usuário no banco de dados
     if (EditarUtilizador($pdo, 4, $utilizadorModificado)) {
         $utilizador = ObterUmUtilizador($pdo, 4);
-        echo "<div class='alert alert-success' role='alert'>
-        <span class='glyphicon glyphicon-remove'></span>&nbsp;
-        <strong>Sucesso!</strong></div>";
+        echo "<div class='modal' tabindex='-1' role='dialog'>
+        <div class='modal-dialog' role='document'>
+          <div class='modal-content'>
+            <div class='modal-header'>
+              <h5 class='modal-title'>Editar Utilizador</h5>
+              <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+                <span aria-hidden='true'>&times;</span>
+              </button>
+            </div>
+            <div class='modal-body'>
+              <p>O utilizador foi alterado com sucesso!</p>
+            </div>
+            <div class='modal-footer'>
+              <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>
+            </div>
+          </div>
+        </div>
+      </div>";
     } else {
-        echo "<div class='alert alert-danger' role='alert'>
-        <span class='glyphicon glyphicon-remove'></span>&nbsp;
-        <strong>Ocorreu um erro ao tentar atualizar o produto!</strong></div>";
+        echo "<div class='modal' tabindex='-1' role='dialog'>
+        <div class='modal-dialog' role='document'>
+          <div class='modal-content'>
+            <div class='modal-header'>
+              <h5 class='modal-title'>Editar Utilizador</h5>
+              <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+                <span aria-hidden='true'>&times;</span>
+              </button>
+            </div>
+            <div class='modal-body'>
+              <p>Ocorreu um erro a editar o utilizador</p>
+            </div>
+            <div class='modal-footer'>
+              <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>
+            </div>
+          </div>
+        </div>
+      </div>";
     }
 }
 ?>
@@ -74,120 +104,113 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     include __DIR__ . "/includes/sidebar_perfil.php";
     ?>
 
-    <form class="perfil centro texto_perfil form_editar" method="GET">
+    <form class="centro esquerdo form_editar" method="GET">
         <h3>Perfil do Utilizador</h3>
         <p>Esta é a tua página de perfil de utilizador. Aqui podes ver as tuas informações pessoais e editá-las</p>
 
+        <div class="align-items-md-stretch">
+            <div>
+                <div class="card mb-3">
+                    <div class="card-header">
+                        <h5 class="esquerdo">A minha conta</h5>
+                        <button id="btn_editar" class="btn btn-warning direito" style="width: auto;" type="button"
+                            value="Editar">Editar</button>
+                    </div>
+                    <div class="card-body">
 
-        <div class="pedidos">
-            <div class="align-items-md-stretch">
-                <div>
-                    <div class="card mb-3">
-                        <div class="card-header">
-                            <h5 class="esquerdo">A minha conta</h5>
-                            <button id="btn_editar" class="btn btn-warning direito" style="width: auto;" type="button"
-                                value="Editar">Editar</button>
-                        </div>
-                        <div class="card-body">
-
-                            <p class="cinzento" style="padding:5px">Informação do Utilizador</p>
-                            <div class="esquerdo" style="padding:5px">
-                                <span>Primeiro Nome</span>
-                                <div class="input-group flex-nowrap">
-                                    <input name="nome" readonly type="text" class="form-control"
-                                        placeholder="Primeiro Nome" aria-label="Primeiro Nome"
-                                        aria-describedby="addon-wrapping" value="<?php if (!empty($utilizador['nome']))
-                                            echo $utilizador['nome']; ?>">
-                                </div>
-                                <br>
-                                <span>Email</span>
-                                <div class="input-group flex-nowrap">
-                                    <input name="email" readonly type="text" class="form-control" placeholder="Email"
-                                        aria-label="Email" aria-describedby="addon-wrapping" value="<?php if (!empty($utilizador['email']))
-                                            echo $utilizador['email']; ?>">
-                                </div>
-                            </div>
-                            <div class="direito" style="padding:5px">
-                                <span>Apelido</span>
-                                <div class="input-group flex-nowrap">
-                                    <input name="apelido" readonly type="text" class="form-control"
-                                        placeholder="Apelido" aria-label="Apelido" aria-describedby="addon-wrapping"
-                                        value="<?php if (!empty($utilizador['apelido']))
-                                            echo $utilizador['apelido']; ?>">
-                                </div>
-                                <br>
-                                <span>Nº de Telemóvel</span>
-                                <div class="input-group flex-nowrap">
-                                    <input name="telemovel" readonly type="text" class="form-control"
-                                        placeholder="Telemóvel" aria-label="Telemovel" aria-describedby="addon-wrapping"
-                                        value="<?php if (!empty($utilizador['telemovel']))
-                                            echo $utilizador['telemovel']; ?>">
-                                </div>
-                            </div>
-
-                            &emsp;
-                            <hr>&emsp;
-
-                            <p class="cinzento">Morada</p>
-                            <span>Morada</span>
+                        <p class="cinzento" style="padding:5px">Informação do Utilizador</p>
+                        <div class="esquerdo" style="padding:5px">
+                            <span>Primeiro Nome</span>
                             <div class="input-group flex-nowrap">
-                                <input name="morada" readonly type="text" class="form-control" placeholder="Morada"
-                                    aria-label="Morada" aria-describedby="addon-wrapping" value="<?php if (!empty($utilizador['morada']))
-                                        echo $utilizador['morada']; ?>">
+                                <input name="nome" readonly type="text" class="form-control" placeholder="Primeiro Nome"
+                                    aria-label="Primeiro Nome" aria-describedby="addon-wrapping" value="<?php if (!empty($utilizador['nome']))
+                                        echo $utilizador['nome']; ?>">
                             </div>
                             <br>
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <div class="col-md-12 esquerdo">
-                                        <span>Cidade</span>
-                                        <div class="input-group flex-nowrap">
-                                            <input name="cidade" readonly type="text" class="form-control"
-                                                placeholder="Cidade" aria-label="Cidade"
-                                                aria-describedby="addon-wrapping" value="<?php if (!empty($utilizador['cidade']))
-                                                    echo $utilizador['cidade']; ?>">
-                                        </div>
-                                    </div>
-                                </div>
+                            <span>Email</span>
+                            <div class="input-group flex-nowrap">
+                                <input name="email" readonly type="text" class="form-control" placeholder="Email"
+                                    aria-label="Email" aria-describedby="addon-wrapping" value="<?php if (!empty($utilizador['email']))
+                                        echo $utilizador['email']; ?>">
+                            </div>
+                        </div>
+                        <div class="direito" style="padding:5px">
+                            <span>Apelido</span>
+                            <div class="input-group flex-nowrap">
+                                <input name="apelido" readonly type="text" class="form-control" placeholder="Apelido"
+                                    aria-label="Apelido" aria-describedby="addon-wrapping" value="<?php if (!empty($utilizador['apelido']))
+                                        echo $utilizador['apelido']; ?>">
+                            </div>
+                            <br>
+                            <span>Nº de Telemóvel</span>
+                            <div class="input-group flex-nowrap">
+                                <input name="telemovel" readonly type="text" class="form-control"
+                                    placeholder="Telemóvel" aria-label="Telemovel" aria-describedby="addon-wrapping"
+                                    value="<?php if (!empty($utilizador['telemovel']))
+                                        echo $utilizador['telemovel']; ?>">
+                            </div>
+                        </div>
 
-                                <div class="col-md-4">
-                                    <div class="col-md-12">
-                                        <span>País</span>
-                                        <div class="input-group flex-nowrap">
-                                            <input name="pais" readonly type="text" class="form-control"
-                                                placeholder="País" aria-label="País" aria-describedby="addon-wrapping"
-                                                value="<?php if (!empty($utilizador['pais']))
-                                                    echo $utilizador['pais']; ?>">
-                                        </div>
-                                    </div>
-                                </div>
+                        &emsp;
+                        <hr>&emsp;
 
-                                <div class="col-md-4">
-                                    <div class="col-md-12">
-                                        <span>Código-Postal</span>
-                                        <div class="input-group flex-nowrap">
-                                            <input name="CodPostal" readonly type="text" class="form-control"
-                                                placeholder="Código Postal" aria-label="Código Postal"
-                                                aria-describedby="addon-wrapping" value="<?php if (!empty($utilizador['CodPostal']))
-                                                    echo $utilizador['CodPostal']; ?>">
-                                        </div>
+                        <p class="cinzento">Morada</p>
+                        <span>Morada</span>
+                        <div class="input-group flex-nowrap">
+                            <input name="morada" readonly type="text" class="form-control" placeholder="Morada"
+                                aria-label="Morada" aria-describedby="addon-wrapping" value="<?php if (!empty($utilizador['morada']))
+                                    echo $utilizador['morada']; ?>">
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="col-md-12 esquerdo">
+                                    <span>Cidade</span>
+                                    <div class="input-group flex-nowrap">
+                                        <input name="cidade" readonly type="text" class="form-control"
+                                            placeholder="Cidade" aria-label="Cidade" aria-describedby="addon-wrapping"
+                                            value="<?php if (!empty($utilizador['cidade']))
+                                                echo $utilizador['cidade']; ?>">
                                     </div>
                                 </div>
                             </div>
 
+                            <div class="col-md-4">
+                                <div class="col-md-12">
+                                    <span>País</span>
+                                    <div class="input-group flex-nowrap">
+                                        <input name="pais" readonly type="text" class="form-control" placeholder="País"
+                                            aria-label="País" aria-describedby="addon-wrapping" value="<?php if (!empty($utilizador['pais']))
+                                                echo $utilizador['pais']; ?>">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <div class="col-md-12">
+                                    <span>Código-Postal</span>
+                                    <div class="input-group flex-nowrap">
+                                        <input name="CodPostal" readonly type="text" class="form-control"
+                                            placeholder="Código Postal" aria-label="Código Postal"
+                                            aria-describedby="addon-wrapping" value="<?php if (!empty($utilizador['CodPostal']))
+                                                echo $utilizador['CodPostal']; ?>">
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+
                     </div>
                 </div>
             </div>
         </div>
     </form>
     <?php
-    include __DIR__ . "/includes/footer_2.php";
+    //include __DIR__ . "/includes/footer_2.php";
     ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
-    <!--<script src="C:\xampp\htdocs\PTAW_FoodDash\assets\js\perfil.php"></script>-->
 
     <script>
         document.addEventListener("DOMContentLoaded", function () {
@@ -202,8 +225,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 if (btnEditar.innerHTML == "Editar") {
                     btnEditar.innerHTML = "Guardar";
                     btnEditar.setAttribute("type", "button");
-                    btnEditar.style.backgroundColor = "green";
-                    btnEditar.style.color = "white";
+                    btnEditar.classList.remove("btn-warning");
+                    btnEditar.classList.add("btn-success");
                     inputs.forEach(function (input) {
                         input.removeAttribute("readonly");
                     });
@@ -214,8 +237,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 else {
                     btnEditar.innerHTML = "Editar";
                     btnEditar.setAttribute("type", "submit");
-                    btnEditar.style.backgroundColor = "#FEBB41";
-                    btnEditar.style.color = "black";
+                    btnEditar.classList.remove("btn-success");
+                    btnEditar.classList.add("btn-warning");
                     inputs.forEach(function (input) {
                         input.setAttribute("readonly", "readonly");
                     });
