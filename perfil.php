27fe93da8f067ed9d8684954ad20310dacce65ec
Ana Beatriz Vicente <vicente.anab@ua.pt>
2024-05-09 20:53:44 +0100

    corringindo o css do perfil.php

--- perfil.php
+++ perfil.php
@@ -205,7 +205,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         </div>
     </form>
     <?php
-    //include __DIR__ . "/includes/footer_2.php";
+    include __DIR__ . "/includes/footer_2.php";
     ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
