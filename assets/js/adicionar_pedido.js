d5442247d942a6f2638ce4647060b05827efb8f4
newrlkko <166535715+rllko@users.noreply.github.com>
2024-05-19 23:11:15 +0100

    dable para organizar dados inseridos

--- assets/js/adicionar_pedido.js
+++ assets/js/adicionar_pedido.js
@@ -1,53 +1,183 @@
-// complemento
-let complementosContainer = document.getElementById("complement-section");
-let personalizacoesContainer = document.getElementById("personalizations-section");
-const itemSozinhoForm = document.getElementById("itemsozinho-form");
-const personalizacoesAtivasForm = document.getElementById("personalizacoes-ativas-form");
+"use strict";
 
-//modal
-let modal = document.getElementById("modal");
-var btnComplemento = document.getElementById("novoComplementoBtn");
-var btnOpcao = document.getElementById("novaOpcaoBtn");
-var spanBotaoX = document.getElementsByClassName("close")[0];
+// Container complementos
+let complementoContainer = document.querySelector("#complement-section");
+// Container personalizações
+let personalizacoesContainer = document.querySelector("#personalizations-section");
+// Checkbox item sozinho
+const itemSozinhoForm = document.querySelector("#itemsozinho-form");
+// Checkbox personalizações ativas
+const personalizacoesAtivasForm = document.querySelector("#personalizacoes-ativas-form");
 
-//esconder se o botão for não
-itemSozinhoForm.onclick = _ => hideCheckboxElement("itemsozinho",complementosContainer);
-personalizacoesAtivasForm.onclick = _ => hideCheckboxElement("personalizacoesativas",personalizacoesContainer);
+// Caixa modal
+const modal = document.querySelector("#modal");
+// + Novo Complemento
+const btnComplemento = document.querySelector("#novoComplementoBtn");
+// Botão na caixa de input a dizer "Adicionar"
+const adicionarDadosBtn = document.querySelector("#adicionarBtn");
+// Botão de fechar a caixa
+const spanBotaoX = document.querySelectorAll(".close")[0];
+// Input da caixa de colocar dados
+const novoItemInput = document.querySelector("#novoItemInput");
 
-function hideCheckboxElement(name, elementToHide){
-    // pegar na radiobox que esteja selecionada
-    const clicked = document.querySelector('input[name='+`${name}`+']:checked');
-    // usar valor de radiobox e aplicar na visibilidade da caixa
-    elementToHide.style.display = clicked.value == "true" ? 'block' : 'none';
-}
+// Tabela de bebidas
+const addDrinkDableContainer = document.querySelector("#add-drink-dable-container");
+// Importar bebida
+const btnImportarBebida = document.querySelector("#import-drink-btn");
 
-// abrir interface ao clicar no botão, substring para remover o "+ "
-btnComplemento.onclick = event => { 
-    event.preventDefault();
-    showModal(btnComplemento.textContent.substring(2))};
+// Adicionar acompanhamento
+const btnAddAcompanhamento = document.querySelector("#add-acompanhamento-btn");
+// Importar acompanhamento
+const btnImportarAcompanhamento = document.querySelector("#import-drink-btn");
 
+// Adicionar bebida
+const btnNovaBebida = document.querySelector("#add-drink-btn");
+
+// Criar tabela de bebida
+const bebidaDableContainer = "add-drink-dable-container";
+const acompanhamentosDableContainer = "add-personalization-dable-container";
+let dableBebida = new Dable();
+let dableAcompanhamento = new Dable();
+
+const form = document.querySelector("#dataForm");
+
+form.submit = () => console.log("hi");
+
+// colunas das tabelas
+let columns = [ 'Nome','' ];
+// Bebidas, não alterem este array, usem dable.AddRow ou dable.RemoveRow para alterarem os dados
+let dadosBebidas = [ ['Coca-Cola',''],['Ice Tea Manga',''],['Fanta Uva',''],['Sumol Laranja',''] ];
+// Acompanhamentos, não alterem este array, usem dable.AddRow ou dable.RemoveRow para alterarem os dados
+let dadosAcompanhamentos = [ ['Batatas médias',''],['Sopa de ervilhas',''],['5 nuggets',''] ];
+
+configurarDable(dableBebida,bebidaDableContainer,dadosBebidas,columns);
+configurarDable(dableAcompanhamento,acompanhamentosDableContainer,dadosAcompanhamentos,columns);
+
+// Associar botão com tabela usando Map
+const opcoesMap = new Map();
+opcoesMap.set(btnNovaBebida,dableBebida);
+opcoesMap.set(btnImportarBebida,"put import");
+
+opcoesMap.set(btnAddAcompanhamento,dableAcompanhamento);
+opcoesMap.set(btnImportarAcompanhamento,"put import");
+
+// Adiciona evento aos botões de adicionar elemento
 const addButtons = document.querySelectorAll(".modal_event");
 addButtons.forEach(element => {
     element.onclick = event => { 
         event.preventDefault();
-        showModal(element.textContent.substring(2))
+        console.log(element.id)
+        showModal(element.textContent.substring(2),opcoesMap.get(element));
     };
 });
 
-function showModal(text){
-    const modalText = document.querySelector("#modal-text");
-    modalText.textContent = `Adicionar ${text}`;
-    modal.style.display = "flex";
+// Esconder se o botão for não
+itemSozinhoForm.onclick = _ => hideCheckboxElement("itemsozinho",complementoContainer);
+personalizacoesAtivasForm.onclick = _ => hideCheckboxElement("personalizacoesativas",personalizacoesContainer);
+
+// Adiciona funcionalidade a botão de apagar
+document.addEventListener('click', event => {
+  if (event.target.classList.contains('deleteRow')) {
+    buttons(event.target);
+  }
+});
+
+
+function buttons(element) {
+  let dable;
+  switch(element.id){
+    case bebidaDableContainer:
+      dable = dableBebida;
+    break;
+    case acompanhamentosDableContainer:
+      dable = dableAcompanhamento;
+    break;
+  }
+
+  var rowNumber = element.getAttribute('data-rownumber');
+  dable.DeleteRow(rowNumber);
 }
 
-// ao clicar no x do modal, esconder interface de caixa
-spanBotaoX.onclick = _ => {
-  modal.style.display = "none";
-}
+// Abrir interface ao clicar no botão, substring para remover o "+ "
+btnAddAcompanhamento.onclick = function(event) { 
+  console.log("Acompanhamento")
+    event.preventDefault();
+    showModal(btnComplemento.textContent.substring(2),opcoesMap.get(btnAddAcompanhamento))
+  };
 
-// quando o usuario clicar fora da caixa, esconder interface de caixa
+// Ao clicar no x do modal, esconder interface de caixa
+spanBotaoX.onclick = hideModal;
+
+// Quando o usuario clicar fora da caixa, esconder interface de caixa
 window.onclick = event => {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal.classList.add('d-none');
   }
+}
+
+function configurarDable(dable,containerElement,rowArray,columnArray){
+  // Configura tabelas Dable para bebida
+  dable.SetDataAsRows(rowArray);
+  dable.style = 'bootstrap';	//set the style
+  dable.SetColumnNames(columnArray);
+  dable.columnData[1].CustomRendering = function (_cellValue, rowNumber) {
+    return '<button type="button" id="'+containerElement+'" class="deleteRow" data-rownumber="' + rowNumber + '">✕</button>';
+  };
+  dable.BuildAll(containerElement);
+}
+
+/**
+ * 
+ * @param {*} name 
+ * @param {*} elementToHide 
+ */
+function hideCheckboxElement(name, elementToHide){
+  // pegar na radiobox que esteja selecionada
+  const clicked = document.querySelector('input[name='+`${name}`+']:checked');
+  // usar valor de radiobox e aplicar na visibilidade da caixa
+  elementToHide.style.display = clicked.value == "true" ? 'block' : 'none';
+}
+
+/**
+ * 
+ * @param {*} text 
+ * @param {*} list 
+ */
+function showModal(text,dable){
+    const modalText = document.querySelector("#modal-text");
+    modalText.textContent = `Adicionar ${text}`;
+    
+    modal.classList.remove('d-none');
+    modal.classList.add('d-flex');
+
+    adicionarDadosBtn.onclick = function() {
+      dable.AddRow([novoItemInput.value.toString(),'']);
+      spanBotaoX.click()
+      
+      console.log(JSON.stringify({data:tableToJson(dable)}));
+    }
+}
+
+function hideModal(){
+  modal.classList.add('d-none');
+  novoItemInput.value= "";
+}
+
+/**
+ * 
+ * @param {*} dableTable 
+ * @returns 
+ */
+function tableToJson(dableTable) { 
+  let element = document.getElementById(dableTable.id);
+  var data = [];
+  for (var i = 1; i < dableTable.rows.length; i++) { 
+      var cells = element.querySelectorAll('tbody td');
+      var rowData = []; 
+      for (var j = 0; j < cells.length; j+=2) { 
+          rowData.push(cells[j].innerHTML);
+      } 
+      data.push(rowData); 
+  } 
+  return data; 
 }
--- dashboard_adicionar_pedido.php
+++ dashboard_adicionar_pedido.php
@@ -21,6 +21,7 @@ session_start();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/styles/sitecss.css">
 	<link rel="stylesheet" href="assets/styles/dashboard.css">
+  <script src="./assets/js/dable.js"></script>
   </head>
   <body>
   <!--Zona do Header -->
@@ -73,31 +74,30 @@ $idEmpresa = 1;
 	  
 	  <?php include __DIR__."/includes/uploadFotosItens.php"; ?>
 	  
-      <form action="" method="post" enctype="multipart/form-data">
-		<input type="hidden" id="idestabelecimento" name="idestabelecimento" value="<?php echo htmlspecialchars($idEmpresa); ?>">
+    <form action="" method="post" enctype="multipart/form-data" id="dataForm">
+      <input type="hidden" id="idestabelecimento" name="idestabelecimento" value="<?php echo htmlspecialchars($idEmpresa); ?>">
 
-
-        <div class="mb-3">
-          <label for="nome" class="form-label">Nome</label>
-          <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome do item" required>
+          <div class="mb-3">
+            <label for="nome" class="form-label">Nome</label>
+            <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome do item" required>
+          </div>
+      
+          <div class="mb-3">
+            <label for="foto" class="form-label">Foto</label>
+            <input type="file" class="form-control" id="foto" name="foto" accept="image/*" required>
+          </div>
+          
+      <div class="mb-3">
+        <label for="descricaoForm1 " class="purple-text">Descrição</label>
+        <div class="form-group w-100">
+        <textarea placeholder="Introduza Descrição" class="form-control w-100" id="descricao" name="descricao" rows="3"></textarea>
         </div>
-		
-        <div class="mb-3">
-          <label for="foto" class="form-label">Foto</label>
-          <input type="file" class="form-control" id="foto" name="foto" accept="image/*" required>
-        </div>
-        
-		<div class="mb-3">
-		  <label for="descricaoForm1 " class="purple-text">Descrição</label>
-		  <div class="form-group w-100">
-			<textarea placeholder="Introduza Descrição" class="form-control w-100" id="descricao" name="descricao" rows="3"></textarea>
-		  </div>
-		</div>
-		
-		<div class="mb-3">
-          <label for="nome" class="form-label">Preço</label>
-          <input type="number" class="form-control" id="preco" name="preco" placeholder="Digite um número" step="0.01" required>
-        </div>
+      </div>
+      
+      <div class="mb-3">
+            <label for="nome" class="form-label">Preço</label>
+            <input type="number" class="form-control" id="preco" name="preco" placeholder="Digite um número" step="0.01" required>
+          </div>
 		
 <?php
 require_once "database/credentials.php";
@@ -108,7 +108,6 @@ try {
 	$stmt = $pdo->prepare("select id_categoria, nome from categorias where id_empresa = ? ");
 	$stmt->execute([$idEmpresa]);
 	$stmt = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 
 	echo "
 		<div class='mb-3'>
@@ -133,55 +132,43 @@ try {
 		<p class=" fw-bold purple-text">Vende-se Item Sozinho?</p>
 		<div class="w-25 mb-4" id="itemsozinho-form">
 			<div class="form-check form-check-inline">
-			<input class="form-check-input" type="radio" id="inlineRadio2" name="itemsozinho" value="false" checked>
+			<input class="form-check-input" type="radio" id="inlineRadio2" name="itemsozinho" value="false" >
 			<label class="form-check-label" for="inlineCheckbox2">Sim</label>
 			</div>
 			<div class="form-check form-check-inline">
-			<input class="form-check-input" type="radio" id="inlineRadio1" name="itemsozinho" value="true" >
+			<input class="form-check-input" type="radio" id="inlineRadio1" name="itemsozinho" value="true" checked>
 			<label class="form-check-label" for="inlineCheckbox1">Não</label>
 			</div>
 		</div>
 
-		
 		<div class="container mb-5" id="complement-section">
-    <p class="h5 fw-bold">Acompanhamento</p>
+      <p class="h5 fw-bold">Complemento</p>
       <button class="btn btn-custom text-left" id="novoComplementoBtn">+ Novo Complemento</button>
 			<div class="complement-section">
 				<div class="complement-header mb-0">
 					<p class="fw-bold m-0">Bebida</p>
-					<button class="btn btn-custom modal_event" style="margin-left: auto;">+ Nova Opção</button>
+					<button class="btn btn-custom modal_event" style="margin-left: auto;" id="add-drink-btn">+ Nova Opção</button>
           <span style="margin-left: 0.5vw;margin-right:0.5vw"> ou </span>
-          <button class="btn btn-custom">Importar Existente</button>
+          <button class="btn btn-custom" id="import-drink-btn">Importar Existente</button>
 				</div>
 				<hr class="mt-0">
-				<ul class="list-unstyled">
-					<li>Coca-Cola</li>
-					<li>Ice Tea Manga</li>
-					<li>Fanta Uva</li>
-					<li>Sumol Laranja</li>
-					<li>Água Mineral</li>
-				</ul>
-			</div>
+        <div id="add-drink-dable-container"></div>
+    </div>
 
-			<div class="complement-section">
-				<div class="complement-header mb-0">
-          <p class="fw-bold m-0">Acompanhamento</p>
-					<button class="btn btn-custom modal_event" style="margin-left: auto;">+ Nova Opção</button>
-          <span style="margin-left: 0.5vw;margin-right:0.5vw"> ou </span>
-          <button class="btn btn-custom">Importar Existente</button>
-				</div>
+    <div class="complement-section">
+      <div class="complement-header mb-0">
+        <p class="fw-bold m-0">Acompanhamento</p>
+        <button class="btn btn-custom modal_event" style="margin-left: auto;" id="add-acompanhamento-btn">+ Nova Opção</button>
+        <span style="margin-left: 0.5vw;margin-right:0.5vw"> ou </span>
+        <button class="btn btn-custom">Importar Existente</button>
+      </div>
 
-				<hr class="mt-0">
-				<ul class="list-unstyled">
-					<li>Batatas médias</li>
-					<li>Sopa de ervilhas</li>
-					<li>Batatas rústicas</li>
-					<li>5 nuggets</li>
-				</ul>
-			</div>
-		</div>
+      <hr class="mt-0">
+      <div id="add-personalization-dable-container"></div>
+
+    </div>
+  </div>
 	
-
 	<div>
 		<p class="m fw-bold purple-text ">Personalizações Ativas?</p>
 		<div class="w-25" id="personalizacoes-ativas-form">
@@ -201,7 +188,7 @@ try {
     <div class="section-title">
     <p class="h5 fw-bold ">Hamburguer</p>
     </div>
-    <button type="button" class="btn btn-custom modal_event" >+ Nova Opção</button>
+    <button type="button" class="btn btn-custom modal_event" id="nova-personalizacao-btn">+ Nova Opção</button>
     <hr>
     <div class="option-list">
       <div class="option-row">
@@ -253,11 +240,11 @@ try {
 		</div>
 	</div>
 	<button type="submit" class="btn btn-primary" style="width: 40%; margin: 2% 30%;">Adicionar Item</button>
-  </form>
 </div>
+</form>
   
 <!-- Modal -->
-<div id="modal" class="modal">
+<div id="modal" class="modal d-none">
   <div class="modal-content">
     <span class="close">&times;</span>
     <p class="fw-bold mt-1 mb-2" id="modal-text">a culpa é do richard</p>
@@ -268,6 +255,7 @@ try {
 
   <!--Zona do Footer -->
   <?php include __DIR__."/includes/footer_2.php"; ?>
+
   <script src="./assets/js/adicionar_pedido.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
